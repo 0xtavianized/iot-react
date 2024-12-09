16b3fb89718c2c4488dc01738a2c9c7e42ac8128
0xtavianized <oktavian@stmik-wp.ac.id>
2024-12-09 23:53:59 +0700

    cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
-import tailwindCssAnimate from "tailwindcss-animate";
+const tailwindCssAnimate = require("tailwindcss-animate");
 
-export default {
+module.exports = {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
